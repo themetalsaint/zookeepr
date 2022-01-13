34cc406d516bc86bdf45316e98e129197d1a4716
Martin <mflores0572@gmail.com>
2022-01-13 13:06:23 -0500

    resolved the error where i wouldnt get the data back due to some mistype error

--- server.js
+++ server.js
@@ -1,31 +1,19 @@
-// required packages
 const express = require("express");
+const { animals } = require("./data/animals");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require("./data/animals.json");
 
-// start filter query function
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
-  // Note that we save the animalsArray as filteredResults here:
   let filteredResults = animalsArray;
   if (query.personalityTraits) {
-    // Save personalityTraits as a dedicated array.
-    // If personalityTraits is a string, place it into a new array and save.
     if (typeof query.personalityTraits === "string") {
       personalityTraitsArray = [query.personalityTraits];
     } else {
       personalityTraitsArray = query.personalityTraits;
     }
-    // Loop through each trait in the personalityTraits array:
     personalityTraitsArray.forEach((trait) => {
-      // Check the trait against each animal in the filteredResults array.
-      // Remember, it is initially a copy of the animalsArray,
-      // but here we're updating it for each trait in the .forEach() loop.
-      // For each trait being targeted by the filter, the filteredResults
-      // array will then contain only the entries that contain the trait,
-      // so at the end we'll have an array of animals that have every one
-      // of the traits when the .forEach() loop is finished.
       filteredResults = filteredResults.filter(
         (animal) => animal.personalityTraits.indexOf(trait) !== -1
       );
@@ -46,19 +34,14 @@ function filterByQuery(query, animalsArray) {
       (animal) => animal.name === query.name
     );
   }
-  // return the filtered results:
   return filteredResults;
 }
-// end filter query function
 
-//Start findByID function
 function findById(id, animalsArray) {
   const result = animalsArray.filter((animal) => animal.id === id)[0];
   return result;
 }
-//End findByID function
 
-//Start Get Routes
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -68,7 +51,6 @@ app.get("/api/animals", (req, res) => {
 });
 
 app.get("/api/animals/:id", (req, res) => {
-  let result = animals;
   const result = findById(req.params.id, animals);
   if (result) {
     res.json(result);
@@ -76,9 +58,7 @@ app.get("/api/animals/:id", (req, res) => {
     res.send(404);
   }
 });
-//End GET Routes
 
-// bottom of app.js
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
