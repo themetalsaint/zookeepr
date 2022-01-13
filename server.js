c075ec81d48133032368794d702af4da975fbd7c
Martin <mflores0572@gmail.com>
2022-01-13 14:00:51 -0500

    added a dataVerifivation function to the server

--- server.js
+++ server.js
@@ -66,6 +66,24 @@ function createNewAnimal(body, animalsArray) {
 }
 // End createNewAnimal Function
 
+// Start validateAnimal function
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+// End validateAnimal function
+
 // Start Get Routes
 app.get("/api/animals", (req, res) => {
   let results = animals;
@@ -90,10 +108,13 @@ app.post("/api/animals", (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // add animal to json file and animals array in this function
-  const animal = createNewAnimal(req.body, animals);
-
-  res.json(animal);
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
 // End POST Route
 
