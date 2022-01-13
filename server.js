88cf1dba913a00f83ad73a9932287c8b89372032
Martin <mflores0572@gmail.com>
2022-01-13 13:57:08 -0500

    added functionality to the createNewAnimal function so that we could update our animalArray with a new animal without it overwriting any existing data

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Necessary Packages
 const express = require("express");
 const { animals } = require("./data/animals");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -54,8 +56,13 @@ function findById(id, animalsArray) {
 
 // Start createNewAnimal Function
 function createNewAnimal(body, animalsArray) {
-  console.log(body);
-  return body;
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
 }
 // End createNewAnimal Function
 
@@ -80,8 +87,13 @@ app.get("/api/animals/:id", (req, res) => {
 
 // Start POST Route
 app.post("/api/animals", (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // add animal to json file and animals array in this function
+  const animal = createNewAnimal(req.body, animals);
+
+  res.json(animal);
 });
 // End POST Route
 
